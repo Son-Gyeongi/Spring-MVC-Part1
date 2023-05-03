84afdb9251fb47f0771f594f4171dbeffcd83433
Son-Gyeongi <thsruddl77@gmail.com>
2023-05-04 00:05:22 +0900

    스프링 부트에 서블릿 환경 구성

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,15 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * springBoot에서 서블릿을 쓸 수 있게 지원
+ * @ServletComponentScan 애노테이션 지원
+ * 스프링이 자동으로 내 패키지(hello.servlet) 포함해서 하위 패키지를 다 뒤져서 서블릿을 다 찾아서
+ * 자동으로 서블릿에 등록해서 실행할 수 있도록 도와준다.
+ */
+@ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
