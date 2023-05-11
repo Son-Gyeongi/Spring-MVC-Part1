9436d14a6e8d6ed6325a9246f4b3e5ec67ced370
Son-Gyeongi <thsruddl77@gmail.com>
2023-05-11 19:36:30 +0900

    MVC 패턴 - 회원 저장 (컨트롤러)

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -18,12 +18,14 @@ public class MvcMemberSaveServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 1. 파라미터 받고
         // 값을 가져와서 읽어야 한다.
         // url의 쿼리 스트링이든 HTML-form(POST방식) 데이터든 username=kim&&age=20 이거를 읽어야 한다.
         String username = request.getParameter("username");
         // request.getParameter의 응답 결과는 항상 문자이다.  숫자타입으로 변환해주자.
         int age = Integer.parseInt(request.getParameter("age"));
 
+        // 2. 실제 비즈니스 로직 호출하고
         // Member 객체 만들기
         Member member = new Member(username, age);
         // 저장하기
@@ -38,9 +40,11 @@ public class MvcMemberSaveServlet extends HttpServlet {
          *
          * 아래에 JSP View가 나와야 한다.
          */
+        // 3. Model에 데이터 담고
         // request 객체 내부에 Map같은 내부 저장소가 있다. 거기에 member가 저장이 된다.
         request.setAttribute("member", member);
 
+        // 4. view로 던지고 컨트롤러의 역할을 충실하게 하고 있다.
         String viewPath = "/WEB-INF/views/save-result.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);// 이 경로로 넘어가겠다.
         dispatcher.forward(request, response); // 내부에서 호출
