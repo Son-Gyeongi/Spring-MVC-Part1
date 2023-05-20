21f7c256be3611ba0492b35f4ef564da3c8ce1ba
Son-Gyeongi <thsruddl77@gmail.com>
2023-05-20 19:58:24 +0900

    스프링 MVC 실용적인 방식 - @RequestMapping (SpringMemberControllerV3, String 반환)

---
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,48 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 회원 등록 폼
+    @RequestMapping("/new-form") // 매핑 URL이 겹치면 오류난다.
+    public String newForm() {
+        return "new-form"; // viewName만 반환
+    }
+
+    // 회원 저장
+    @RequestMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age, // 타입 캐스팅, 타입 변환까지 스프링에서 자동으로 처리해준다.
+            Model model) {
+
+        // 비즈니스 로직
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        // 파라미터로 넘어온 model에 member를 넣어주면 된다.
+        model.addAttribute("member", member);
+        return "save-result"; // viewName만 반환
+    }
+
+    // 회원 목록
+    @RequestMapping // /springmvc/v3/members
+    public String members(Model model) {
+
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members"; // viewName만 반환
+    }
+}
