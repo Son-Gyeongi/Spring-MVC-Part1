37020babd35a25cd7eb90603f85dd38f0ff6412c
Son-Gyeongi <thsruddl77@gmail.com>
2023-05-20 17:07:32 +0900

    스프링 MVC 시작하기 - @Controller (회원 목록)

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+// MemberListControllerV3랑 거의 비슷하다.
+// 회원 목록
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+//        mv.getModel().put("members", members);
+        mv.addObject("members", members);
+        return mv;
+    }
+}
